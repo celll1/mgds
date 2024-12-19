cf25d976c7d3c6cefcdb7c81e7eee1a0a6a3a824
celll1 <cellione06@gmail.com>
2024-12-20 01:36:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='mgds',
-      version='0.1',
-      description='Modular Pytorch DataSet implementation',
-      author='Nerogar',
-      url='https://github.com/Nerogar/stable-diffusion-dataloader/',
-      package_dir={'': 'src'},
-      packages=['mgds'],
-      )
+setup(
+    name='mgds',
+    version='0.1',
+    description='Modular Pytorch DataSet implementation',
+    author='Nerogar',
+    url='https://github.com/Nerogar/stable-diffusion-dataloader/',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+)
